988e93b47019a1ee44376d360844d19e1f109187
luis-garridodev <proganalistaluis@gmail.com>
2022-08-19 17:25:37 -0300

    trabalho de faculdade

---
+++ tarefa python/trabalho.py
@@ -0,0 +1,39 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#questão1
+10/3
+3.3333333333333335
+#questao2
+13*1
+13
+13*2
+26
+13*3
+39
+13*4
+52
+13*5
+65
+13*6
+78
+13*7
+91
+13*8
+104
+13*9
+117
+13*10
+130
+#questão3
+(6*4)%75
+24
+(6*6*6*6)%75
+21
+24%75
+24
+#questão4
+raio=float(input('entre com o valor do raio,para obter a circunferencia'))
+entre com o valor do raio,para obter a circunferencia2
+circunferencia=2*3.14*raio
+print('a circunferencia do circulo{:.2f}'.format(circunferencia))
+a circunferencia do circulo12.56
